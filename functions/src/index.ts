f639ffafdeb59e7eade128c744b8f42a4234d372
goggle555 <goggle555@fives.tech>
2024-12-18 00:51:40 +0900

    feat: discord.jsのサンプルコードを追加する

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,15 @@
 
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { Client, GatewayIntentBits } from "discord.js";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
+  const client = new Client({ intents: [GatewayIntentBits.MessageContent] });
+  client.login("TOKEN");
+
   logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
